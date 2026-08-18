037c661a32b5d58c1f80c2c149290ef4f81a372a
LuisGodoy123 <lagi@cesar.school>
2026-08-18 09:07:54 -0300

    implementando: Tokenizar e ComandoTask

--- processflow.c
+++ processflow.c
@@ -19,3 +19,41 @@ Tarefa *BuscarTarefa(Shell *ProcessFlow, const char *Nome) {
     }
     return NULL;
 }
+
+int Tokenizar(char *Linha, char **Tokens, int MaximoTokens) {
+    int Quantidade = 0;
+    char *Token = strtok(Linha, " \t\r\n");
+    while (Token != NULL && Quantidade < MaximoTokens - 1) {
+        Tokens[Quantidade++] = Token;
+        Token = strtok(NULL, " \t\r\n");
+    }
+    Tokens[Quantidade] = NULL;
+    return Quantidade;
+}
+
+void ComandoTask(Shell *ProcessFlow, char **Argumentos, int QuantidadeArgumentos) {
+    if (QuantidadeArgumentos < 3) {
+        fprintf(stderr, "task, nome, programa,argumentos...\n");
+        return;
+    }
+    if (ProcessFlow->QuantidadeTarefas >= MAXIMO_TAREFAS) {
+        fprintf(stderr, "numero maximo de tarefas atingido\n");
+        return;
+    }
+
+    Tarefa *NovaTarefa = &ProcessFlow->Tarefas[ProcessFlow->QuantidadeTarefas];
+    strncpy(NovaTarefa->Nome, Argumentos[1], TAMANHO_MAXIMO_NOME - 1);
+    NovaTarefa->Nome[TAMANHO_MAXIMO_NOME - 1] = '\0';
+    NovaTarefa->ArquivoEntrada = NULL;
+    NovaTarefa->ArquivoSaida = NULL;
+    NovaTarefa->ModoAnexar = 0;
+
+    NovaTarefa->QuantidadeArgumentos = 0;
+    for (int Indice = 2; Indice < QuantidadeArgumentos && NovaTarefa->QuantidadeArgumentos < MAXIMO_TOKENS - 1; Indice++) {
+        NovaTarefa->Argumentos[NovaTarefa->QuantidadeArgumentos++] = Argumentos[Indice];
+    }
+    NovaTarefa->Argumentos[NovaTarefa->QuantidadeArgumentos] = NULL;
+
+    ProcessFlow->QuantidadeTarefas++;
+    printf("tarefa '%s' cadastrada\n", NovaTarefa->Nome);
+}
